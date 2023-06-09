f3997dbdebcab52a8b7db4a68d75244ceae69f31
Giallored <pacciarelli.1816493@studenti.uniroma1.it>
2023-06-09 11:42:48 +0200

    now read-sensor node pub visible obstacles (positions)

--- autonomous_controllers/scripts/read_sensors.py
+++ autonomous_controllers/scripts/read_sensors.py
@@ -2,8 +2,9 @@
 import rospy
 from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
-from src.utils import FakeLaserScanner,Obstacles,TIAgo
+#from utils import FakeLaserScanner,Obstacles,TIAgo
 from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import PoseArray,Pose
 import numpy as np
 from scipy.spatial.transform import Rotation
 
@@ -13,7 +14,8 @@ class Sensor():
         #self.laser_scanner=Fake_LaserScanner()
         self.tiago=TIAgo()
         self.scanner=FakeLaserScanner()
-        #self.subscriber = rospy.Subscriber("/gazebo/model_states",ModelStates, self.callback)
+        self.pub = rospy.Publisher('obstacle_poses', PoseArray, queue_size=10)
+
 
     def main(self):
         rospy.Subscriber("/gazebo/model_states",ModelStates, self.callback)
@@ -29,9 +31,128 @@ class Sensor():
 
         obstacles = Obstacles(obs_ids,obs_poses)
         visible_obs_pos,visible_obs_id = self.scanner.get_visible_obs(self.tiago,obstacles)
-        print(len(visible_obs_id),'obstacles are visible')
-        print(visible_obs_id)
-        print('------------------')
+        #print(visible_obs_pos)
+        obstacle_poses = PoseArray()
+        obstacle_poses.poses=visible_obs_pos
+        self.pub.publish(obstacle_poses)
+        #print(len(visible_obs_id),'obstacles are visible')
+        #print(visible_obs_pos)
+        #print('------------------')
+
+
+class FakeLaserScanner():
+    def __init__(
+        self, 
+        angle_min=-1.9198600053787231,
+        angle_max=1.9198600053787231,
+        range_min=0.0,
+        range_max=25.0
+        ):
+        self.angle_min = angle_min
+        self.angle_max = angle_max
+        self.range_min = range_min
+        self.range_max = range_max
+
+    def get_visible_obs(self,tiago,obstacles):
+        visible_obs_pos = []
+        visible_obs_id = []
+        obs_ids = obstacles.ids
+        for id in obs_ids:
+            obs_i = obstacles.dict[id]
+            is_visible,rel_pos_i = self.check_visibility(tiago,obs_i)
+            if is_visible:
+                visible_obs_pos.append(list_to_pose(rel_pos_i,[0]*4))
+                visible_obs_id.append(id)
+            
+        return visible_obs_pos,visible_obs_id
+    
+    def check_visibility(self,tiago,obs):
+        tiago_pos = np.array(tiago.mb_position)
+        pos_i= np.array(obs.position)
+        tf = tiago.Tf_tiago_w
+        rel_pos_i = homoProd(np.linalg.inv(tf),pos_i)
+        range_i=np.linalg.norm(rel_pos_i)
+        if range_i<self.range_min or range_i>self.range_max:
+            return False,None
+        bear_i = np.arctan2(rel_pos_i[1],rel_pos_i[0])
+        if bear_i<self.angle_min or bear_i>self.angle_max:
+            return False,None
+        return True,list(rel_pos_i)
+
+class TIAgo():
+    def __init__(self, mb_position=[0.,0.,0.],mb_orientation=[0.,0.,0.]):
+        self.mb_position=mb_position # wrt RFworld
+        self.mb_orientation=mb_orientation # wrt RFworld
+        self.Tf_tiago_w = np.zeros((4,4))
+        #self.Tf_tiago_world
+
+    def set_MBpose(self,new_pos,new_quat):
+        self.mb_position=Vec3_to_list(new_pos)
+        #turn orientation from quat to euler (in rad)
+        rot = Rotation.from_quat(Vec4_to_list(new_quat))
+        self.Tf_tiago_w = Pose2Homo(rot.as_matrix(),self.mb_position)
+        self.mb_orientation=rot.as_euler('xyz', degrees=False)
+
+    def get_MBpose(self):
+        return self.mb_position+self.mb_position
+    
+
+class Obstacles:
+    def __init__(self,obstacles_id, poses):
+        self.ids=obstacles_id
+        self.n_obs=len(self.ids)
+        self.dict=dict()  #wrt RFworld
+        self.set_poses(poses)
+
+    def set_poses(self,poses):
+        for i in range(self.n_obs):
+            id_i=self.ids[i]
+            pos_i=Vec3_to_list(poses[i].position)
+            or_i = Vec4_to_list(poses[i].orientation)
+            self.dict[id_i]= Object(id_i,pos_i,or_i)
+
+
+class Object():
+    def __init__(self,id, position, orientation):
+        self.id=id
+        self.position=position
+        self.orientation=orientation
+    
+    def get_relative_pos(self,Tf):
+        return Tf*self.position
+
+
+def homoProd(Tf,vec3):
+    vec4 = np.append(vec3,[1])
+    prod = np.dot(Tf,vec4)
+    return prod[:-1]
+    
+
+def Vec3_to_list(vector):
+    return [vector.x,vector.y,vector.z]
+def Vec4_to_list(vector):
+    return [vector.x,vector.y,vector.z,vector.w]
+
+def list_to_pose(position,orientation):
+    pose=Pose()
+    pose.position.x = position[0]
+    pose.position.y = position[1]
+    pose.position.z = position[2]
+    pose.orientation.x = orientation[0]
+    pose.orientation.y = orientation[1]
+    pose.orientation.z = orientation[2]
+    pose.orientation.w = orientation[3]
+    return pose
+
+def Pose2Homo(rot,trasl):
+    p=np.append(trasl,[1])
+    M=np.row_stack((rot,[0,0,0]))
+    return np.column_stack((M,p))
+
+
+
+
+
 
 
 if __name__ == '__main__':
